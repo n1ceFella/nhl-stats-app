67ac7961fc0fbfa0621eb926ee7785a44f1017d0
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2023-05-03 21:59:08 -0400

    register

---
+++ src/Components/RegisterForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './RegisterForm.css';
+
+
+function RegisterForm() {
+    return (
+        <div className="register-form">
+        </div>
+    );
+}
+
+export default RegisterForm;
