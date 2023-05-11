5afece9257c3160acfae4ab023a2249282c0230b
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2023-05-10 22:42:10 -0400

    player bar

---
+++ src/MenuBars/PlayerBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './PlayerBar.css';
+
+
+function PlayerBar() {
+    return (
+        <div className="player-bar">
+        </div>
+    );
+}
+
+export default PlayerBar;
