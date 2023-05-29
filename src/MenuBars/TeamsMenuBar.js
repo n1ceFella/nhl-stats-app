a98b2cae784e2763c678a45513fcf00a3fa87a21
Volodymyr Labliuk <50242030+n1ceFella@users.noreply.github.com>
2023-05-29 13:53:42 -0400

    Update TeamsMenuBar.js

--- src/MenuBars/TeamsMenuBar.js
+++ src/MenuBars/TeamsMenuBar.js
@@ -10,7 +10,7 @@ function TeamsMenuBar() {
         const fetchStandings = async () => {
             try {
                 let teamsData = [];
-                const response = await axios.get('/teams'); //'https://wild-puce-seagull-gown.cyclic.app/teams'
+                const response = await axios.get('https://wild-puce-seagull-gown.cyclic.app/teams'); //'https://wild-puce-seagull-gown.cyclic.app/teams'
                 teamsData = response.data.teams;
                 var logos = [
                     {teamName: "Anaheim Ducks",url: "https://content.sportslogos.net/logos/1/1736/full/1651_anaheim_ducks-primary-20141.png" },
